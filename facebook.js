e5bb863cfe3633bfb9338fe892cdb364e5ae7105
ukasyahf462 <32339489+ukasyahf462@users.noreply.github.com>
2018-04-11 00:31:34 +0700

    Add files via upload

---
+++ facebook.js
@@ -0,0 +1,1 @@
+(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id;js.async=true; js.src = &quot;//connect.facebook.net/en_US/all.js#xfbml=1&quot;; fjs.parentNode.insertBefore(js, fjs); }(document, &#39;script&#39;, &#39;facebook-js
